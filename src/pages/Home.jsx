ba424520e2aff035d2c0946cd82ed8a09af29443
Sasha <lxudrr@gmail.com>
2024-06-27 19:38:39 +0200

    feat: implement Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,53 @@
+import { Suspense, useRef } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Environment, OrbitControls } from "@react-three/drei";
 import '../styles/home.scss';
+
+const Cosmo = () => {
+  const gltf = useLoader(GLTFLoader, "./Cosmo/scene.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={1.2} position={[0, -1.2, 0]} />
+    </>
+  );
+};
+
+function AnimatedCosmo() {
+  const cosmoRef = useRef();
+
+  useFrame(() => {
+    if (cosmoRef.current) {
+      cosmoRef.current.rotation.y += 0.005;
+    }
+  });
+
+  return (
+    <group ref={cosmoRef}>
+      <Cosmo />
+    </group>
+  );
+}
 
 const Home = () => {
   return (
     <main className="Home">
       <div className="Home__text-block">
         <div className="Home__title">
-          <h2>0xSasha</h2>
+          <h2 className="Home__title__item">0xSasha</h2>
         </div>
         <div className="Home__description">
-          <p>Software Engineer</p>
+          <p className="Home__description__item">Software Engineer</p>
         </div>
       </div>
+      <Canvas className="Home__container3D">
+        <ambientLight intensity={1} />
+        <OrbitControls enableZoom={false} />
+        <Suspense fallback={null}>
+          <AnimatedCosmo />
+        </Suspense>
+        <Environment preset='sunset' />
+      </Canvas>
     </main>
   )
 }
