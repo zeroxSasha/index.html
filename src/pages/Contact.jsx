d73d3cd1a61f0e6f8fc826f10a4469844b0bb285
Sasha <lxudrr@gmail.com>
2024-07-02 22:05:19 +0200

    feat: update logic

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,29 +10,35 @@ const Contact = () => {
   const [hover, setHover] = useState(null);
 
   const socials = [
-    {name: GitHub, onHover: "#908caa", linkto: "https://github.com/0xSashaa"},
-    {name: Twitter, onHover: "#1DA1F2", linkto: "https://twitter.com/0xSashaa"},
-    {name: LinkedIn, onHover: "#0077b5", linkto: "https://www.linkedin.com/in/0xsasha/"},
-    {name: MailTo, onHover: "#eb6f92", linkto: "mailto:lxudrr@gmail.com"}
+    {name: GitHub, borderColor: "7px solid #42a2fd", onHover: "#908caa", linkto: "https://github.com/0xSashaa"},
+    {name: Twitter, borderColor: "7px solid #38e54d", onHover: "#1DA1F2", linkto: "https://twitter.com/0xSashaa"},
+    {name: LinkedIn, borderColor: "7px solid #fdff00", onHover: "#0077b5", linkto: "https://www.linkedin.com/in/0xsasha/"},
+    {name: MailTo, borderColor: "7px solid #b00020", onHover: "#eb6f92", linkto: "mailto:lxudrr@gmail.com"}
   ]
 
   return (
     <section className="Contact">
-      <ul className="Contact__list">
-        {socials.map((item, index) => (
-          <li key={index} className="Contact__item">
-            <a href={item.linkto} target="_blank">
-              <button className="Contact__button"
-                onMouseEnter={() => setHover(index)}
-                onMouseLeave={() => setHover(null)}
-              >
-                <item.name color={ hover === index ? item.onHover : "#ffffff" } />
-                <p style={{ color: hover === index ? item.onHover : "#ffffff" }}>{ item.name.name.toLowerCase() }</p>
-              </button>
-            </a>
-          </li>
-        ))}
-      </ul>
+      <div className="Contact__text">
+        <h2 className='Contact__title'>CONTACT</h2>
+      </div>
+      <div className="Contact__links">
+        <ul className="Contact__list">
+          {socials.map((item, index) => (
+            <li key={index} className="Contact__item">
+              <a href={item.linkto} target="_blank">
+                <button className="Contact__button"
+                  onMouseEnter={() => setHover(index)}
+                  onMouseLeave={() => setHover(null)}
+                  style={{ border: item.borderColor }}
+                >
+                  <item.name color={ hover === index ? item.onHover : "#ffffff" } />
+                  <p style={{ color: hover === index ? item.onHover : "#ffffff" }}>{ item.name.name.toLowerCase() }</p>
+                </button>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
