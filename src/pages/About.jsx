41ec5d9453cbcb079a4fe129b546dc3a5013f7dd
Sasha <lxudrr@gmail.com>
2024-07-02 22:03:37 +0200

    feat: finish About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,18 @@
+import '../styles/about.scss';
+
 const About = () => {
     return (
         <section className="About">
             <div className="About__text-block">
-                <div className="About__title">
-                    <h2>ABOUT</h2>
+                <h2 className='About__title'>ABOUT</h2>
+                <div className='About__desc'>
+                    <p>I'm a passionate programmer, who always ready for solving various types of problems. My enthusiasm helps me discover and learn new technologies. You always may feel free to ask me for help.</p>
+                    <p>I was born and raised in Kyiv, Ukraine. In my free time, I usually watch youtube, explore linux, and spend time with my friends.</p>
                 </div>
-                <div className="About__description">
-                    <p>some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text some text </p>
-                </div>
+            </div>
+            <div className="About__skills">
+                <img src="https://skillicons.dev/icons?i=git,linux,nix,cpp,python,bash"/>
+                <img src="https://skillicons.dev/icons?i=javascript,react,html,css,scss"/>
             </div>
         </section>
     )
