e06eeddc8be405e60dd919b1bb18fc2b0f7c7729
Sasha <lxudrr@gmail.com>
2024-07-05 17:40:37 +0200

    feat: implement a missing page

--- src/pages/Missing.jsx
+++ src/pages/Missing.jsx
@@ -1,6 +1,17 @@
+import "../styles/missing.scss"
+import Robot from "../assets/svg/Robot";
+
 const Missing = () => {
   return (
-    <section>Missing</section>
+    <section className="Missing">
+      <div className="Missing__text">
+        <h4 className="Missing__title">Oops!</h4>
+        <p className="Missing__404">404</p>
+      </div>
+      <div className="Missing__svg">
+        <Robot />
+      </div>
+    </section>
   )
 }
 
