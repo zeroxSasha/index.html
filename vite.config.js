cd509cf0d28563721244b89168bded6251761b17
Sasha <lxudrr@gmail.com>
2024-07-06 15:38:28 +0200

    feat: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/0xsasha/"
+  base: "/0xSasha/",
+  server: {
+    historyApiFallback: true
+  }
 })
